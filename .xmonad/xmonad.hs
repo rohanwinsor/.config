f5a147f0400dba38eeb180c759bd750032ff5dcc
ahnorr <rohan.w.charles@gmail.com>
2021-05-08 15:21:28 +0530

    XMONAD CHANGES

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,8 +8,11 @@
 --
 
 import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run
 import Data.Monoid
 import System.Exit
+import XMonad.Layout.Gaps
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
@@ -181,7 +184,7 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = tiled ||| Mirror tiled ||| Full
+myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
@@ -250,7 +253,9 @@ myStartupHook = return ()
 
 -- Run xmonad with the settings you specify. No need to modify this.
 --
-main = xmonad defaults
+main = do
+    xmproc <- spawnPipe "xmobar -x 0 ~/.config/xmobar/xmobarrc"
+    xmonad $ docks defaults
 
 -- A structure containing your configuration settings, overriding
 -- fields in the default config. Any you don't override, will
